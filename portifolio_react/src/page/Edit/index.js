1d1e195f0bf0f0887047efb1aa88011edcef5319
Vitor Dahmer <vitor.dahmer@hotmail.com>
2022-12-05 11:51:25 -0300

    adicionado arquicos de deploy

---
+++ portifolio_react/src/page/Edit/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CourseForm from '../../components/CourseForm';
+
+// import { Container } from './styles';
+
+function Edit() {
+  return <>
+    <CourseForm/>
+  </>;
+}
+
+export default Edit;
